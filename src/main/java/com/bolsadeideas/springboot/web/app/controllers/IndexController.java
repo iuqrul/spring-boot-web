f6085acf6e651d848d39d63640d29e020726b7e0
iuqrul <iuqrul@gmail.com>
2019-09-22 13:23:40 +0200

    Hola Spring

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,9 +1,22 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
 	
+		return index();
+	}
+
+	@GetMapping({"/home", "/index"})
+	public String index() {
+	
+		return "index";
+	}
 }
