1af55180c3dca3c4aa307a779c86af52760dab6d
iuqrul <iuqrul@gmail.com>
2019-09-22 18:05:12 +0200

    Templates / 13.Pasar datos a la vista

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,22 +1,45 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
+	public String home(ModelMap model) {
 	
-		return index();
+		model.addAttribute("titulo", "Hola Spring Framework con ModelMap!");
+		return "index";
+	}
+
+	@GetMapping({"/indexmap"})
+	public String index(Map<String, Object> map) {
+	
+		map.put("titulo", "Hola Spring Framework con Map!");
+		return "index";
 	}
 
 	@GetMapping({"/home", "/index"})
-	public String index() {
+	public String index(Model model) {
 	
+		model.addAttribute("titulo", "Hola Spring Framework!");
 		return "index";
 	}
+	
+	@GetMapping({"/indexmv"})
+	public ModelAndView index(ModelAndView mv) {
+	
+		mv.addObject("titulo", "Hola Spring Framework con ModelAndView!");
+		mv.setViewName("index");
+		return mv;
+	}
+	
 }
